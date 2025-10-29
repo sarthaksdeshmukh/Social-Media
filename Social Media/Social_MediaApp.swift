52905a7b5f2110302b06f318cbfa484c4b767bf6
Sarthak Deshmukh <sarthak.deshmukh@yhworks.com>
2025-10-29 19:37:12 +0530

    Initial Commit

---
+++ Social Media/Social_MediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_MediaApp.swift
+//  Social Media
+//
+//  Created by Sarthak Deshmukh on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Social_MediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
